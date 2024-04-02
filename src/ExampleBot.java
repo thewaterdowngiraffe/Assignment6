f838335895912f9b262805ace7176ceaf6531b05
keegan <keegan@SENIORBOI>
2024-04-02 14:37:36 -0400

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

---
+++ src/ExampleBot.java
@@ -0,0 +1,61 @@
+
+import battleship.*;
+
+import java.awt.Point;
+import java.util.Random;
+
+/**
+ * A Sample random shooter - Takes no precaution on double shooting and has no strategy once
+ * a ship is hit - This is not a good solution to the problem!
+ *
+ * @author mark.yendt@mohawkcollege.ca (Dec 2021)
+ */
+public class ExampleBot implements BattleShipBot {
+    private int gameSize;
+    private BattleShip2 battleShip;
+    private Random random;
+
+    /**
+     * Constructor keeps a copy of the BattleShip instance
+     * Create instances of any Data Structures and initialize any variables here
+     * @param b previously created battleship instance - should be a new game
+     */
+
+    @Override
+    public void initialize(BattleShip2 b) {
+        battleShip = b;
+        gameSize = b.BOARD_SIZE;
+
+        // Need to use a Seed if you want the same results to occur from run to run
+        // This is needed if you are trying to improve the performance of your code
+
+        random = new Random(0xAAAAAAAA);   // Needed for random shooter - not required for more systematic approaches
+    }
+
+    /**
+     * Create a random shot and calls the battleship shoot method
+     * Put all logic here (or in other methods called from here)
+     * The BattleShip API will call your code until all ships are sunk
+     */
+
+    @Override
+    public void fireShot() {
+
+        int x = random.nextInt(gameSize);
+        int y = random.nextInt(gameSize);
+
+        // Will return true if we hot a ship
+        boolean hit = battleShip.shoot(new Point(x,y));
+    }
+
+    /**
+     * Authorship of the solution - must return names of all students that contributed to
+     * the solution
+     * @return names of the authors of the solution
+     */
+
+    @Override
+    public String getAuthors() {
+        return "Mark Yendt (CSAIT Professor)";
+    }
+}
